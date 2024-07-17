7ba0b7c2c9ef02f303c9ad7cfe08cef8f1a6423b
AndreiParquez <131967438+AndreiParquez@users.noreply.github.com>
2024-07-17 13:41:26 +0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,8 +52,8 @@ const Navbar = () => {
                     </li>
                 </ul>
             </div>
-            <div className={`fixed top-0 left-0 w-full h-full darkblue flex flex-col justify-center items-start transform ${nav ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-20`}>
-            <div className="bg-zinc-900  mt-4   w-full overflow-hidden">
+            <div className={`fixed top-0 left-0 w-full h-full darkblue flex flex-col justify-between items-start transform ${nav ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-20`}>
+            <div className="bg-zinc-900  my-auto   w-full overflow-hidden">
                 <div className="bg-cover bg-center  h-28" style={{ backgroundImage: `url(${cover})` }}></div>
                 <div className="p-3">
                     <div className="flex items-center mt-[-60px] space-x-2">
@@ -81,7 +81,7 @@ const Navbar = () => {
                     </div>
                    
                     </div>
-                    <button className="w-1/2 h-11 mx-auto font-bold bg-blue-500 text-white rounded-lg mt-5 flex justify-center items-center">
+                    <button className="w-1/2 h-11 mx-auto font-bold mb-2 bg-blue-500 text-white rounded-lg mt-5 flex justify-center items-center">
                     <PencilIcon className="w-5 h-5 mr-2 font-bold" />
                     Edit Profile
                     </button>
